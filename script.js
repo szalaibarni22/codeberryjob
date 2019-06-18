2078e847b1562354891cd3cc5a53fb18ef276efa
SzalaiBarni <szalaibarni22@users.noreply.github.com>
2019-06-18 10:49:02 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Exercise: Create a to do list application
+
+// todoList function used on button click to get user input and 
+function todoList() {
+	// store user value
+	var item    = document.getElementById('todoInput').value
+	// create a text node from the user input
+	var text    = document.createTextNode(item)
+	// create a li tag
+	var newItem = document.createElement('li')
+	// add the user input to the li tag
+	newItem.appendChild(text)
+	// append the li to the html todoList id tag
+  var NewButton = document.createElement('button')
+	document.getElementById('todoList').appendChild(newItem)
+}
