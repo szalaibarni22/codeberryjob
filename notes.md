07c4439485be41ef65df68de0f18a252747ff2f6
Szalai Barni <50046534+szalaibarni22@users.noreply.github.com>
2019-06-19 10:56:24 +0200

    Add files via upload

---
+++ notes.md
@@ -0,0 +1,26 @@
+# Notes
+
+## Step 1: Setup
+
+- Create the GitHub repo
+- Create the .html, .js files - for now on, we won't need a CSS file
+- try to commit and check if it works well
+
+## Step 2: What are we going to build?
+
+- A To-Do App with:
+   - a title
+   - an input field
+   - an Add button
+   (probably we can do all of these in just the HTML)
+
+   -then we will need:
+      - the button to take action: to make a new list element and show it in the page
+      - we will need a Delete button - which deletes the item
+      - we will need a Complete button - which turns the item in italics, faded, strikethrough format
+      - we can reach that Enter key does the same as a click on the add button
+
+
+## Problems occured during the building process
+ - It's easy to make permanent buttons, but how to make them on the fly in JS?
+ - 
---
+++ script.js
@@ -0,0 +1,54 @@
+
+// todoList function used on button click to get user input and 
+function todoList() {
+	// store user value
+	var item    = document.getElementById('todoInput').value;
+	// create a text node from the user input
+	var text    = document.createTextNode(item);
+	// create a li tag
+	var newItem = document.createElement('li');
+	// add the user input to the li tag
+	newItem.appendChild(text);
+	// append the li to the html todoList id tag
+    
+    document.getElementById('todoList').appendChild(newItem)
+	
+	// Create the delete button
+
+    var deleteButton = document.createElement('button'); // Creates the button.
+    
+    deleteButton.textContent = 'Törlés'; // Sets its text.
+    
+	deleteButton.className = 'deleteButton'; // Adds a class to it.
+    	
+	document.getElementById('todoList').appendChild(deleteButton)
+
+	deleteButton.addEventListener('click', function(){
+		newItem.parentNode.removeChild(newItem);
+		deleteButton.classList.add('hide');
+		completeButton.classList.add('hide')
+	});
+
+	// Create the complete button
+	var completeButton = document.createElement('button'); // Creates the button.
+    
+    completeButton.textContent = 'Kész van!'; // Sets its text.
+    
+	completeButton.className = 'completeButton'; // Adds a class to it.
+    	
+	document.getElementById('todoList').appendChild(completeButton);
+
+	completeButton.addEventListener('click', function(){
+		newItem.classList.add('completed');
+		completeButton.classList.add('hide');
+	});
+};
+
+document.getElementById('add-btn').addEventListener('click', todoList);
+
+input.addEventListener('keyup', function(event) {
+    if (event.key === "Enter") {
+		event.preventDefault();
+        document.getElementById('add-btn').click();
+    }
+});
